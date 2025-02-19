fb6a661b7db8ea3783b3ee2183f3129d6d52a40b
Mikou2761210 <mikou.2761210@gmail.com>
2025-02-19 23:31:26 +0900

    DirtySortList Add

---
+++ MikouTools/CollectionTools/DirtySortCollections/DirtySortList.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikouTools.CollectionTools.DirtySortCollections
+{
+
+    public class DirtySortList<T> : List<T>, IList<T>
+    {
+
+        public bool IsDirty { get; private set; } = false;
+        public IComparer<T>? LastComparer = null;
+
+
+        private void MarkDirty()
+        {
+            IsDirty = true;
+        }
+        public void AcceptChanges()
+        {
+            IsDirty = false;
+        }
+
+
+        public new T this[int index]
+        {
+            get => base[index];
+            set
+            {
+                base[index] = value;
+                MarkDirty();
+            }
+        }
+
+        public new int Count => base.Count;
+
+        public bool IsReadOnly => false;
+
+        public new void Add(T item)
+        {
+            base.Add(item);
+            MarkDirty();
+        }
+
+        public new void AddRange(IEnumerable<T> items)
+        {
+            base.AddRange(items);
+            MarkDirty();
+        }
+
+
+        public new bool Remove(T item)
+        {
+            bool removed = base.Remove(item);
+            if (removed) MarkDirty();
+            return removed;
+        }
+        public new void RemoveRange(int index, int count)
+        {
+            base.RemoveRange(index, count);
+            MarkDirty();
+        }
+
+        public new void RemoveAt(int index)
+        {
+            base.RemoveAt(index);
+            MarkDirty();
+        }
+
+        public new void Insert(int index, T item)
+        {
+            base.Insert(index, item);
+            MarkDirty();
+        }
+        public new void InsertRange(int index, IEnumerable<T> collection)
+        {
+            base.InsertRange(index, collection);
+            MarkDirty();
+        }
+        public new void Reverse(int index, int count)
+        {
+            base.Reverse(index, count);
+            MarkDirty();
+        }
+
+        public new void Clear()
+        {
+            base.Clear();
+            MarkDirty();
+        }
+
+        public new int IndexOf(T item) => base.IndexOf(item);
+
+        public new bool Contains(T item) => base.Contains(item);
+
+
+        public new void CopyTo(T[] array, int arrayIndex) => base.CopyTo(array, arrayIndex);
+
+        public new IEnumerator<T> GetEnumerator() => base.GetEnumerator();
+
+
+
+
+
+
+        public new void Sort() => Sort(0, base.Count, null);
+
+        public new void Sort(IComparer<T>? comparer) => Sort(0, base.Count, comparer);
+
+        public new void Sort(int index, int count, IComparer<T>? comparer)
+        {
+            if (IsDirty || comparer?.Equals(LastComparer) == false)
+            {
+                base.Sort(index, count, comparer);
+                LastComparer = comparer;
+                IsDirty = false;
+            }
+        }
+
+
+    }
+}
--- MikouTools/obj/Debug/net8.0/MikouTools.AssemblyInfo.cs
+++ MikouTools/obj/Debug/net8.0/MikouTools.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MikouTools")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+214919c14e393006f0cfb929b60e3456a5968b4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e45632b710b720599abb7bd14af96b2a4ea8b6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MikouTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MikouTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
