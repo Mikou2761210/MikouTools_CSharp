b5a3758632327730f64446fab389ba58736c350d
Mikou2761210 <mikou.2761210@gmail.com>
2025-03-08 16:18:55 +0900

    Delete unnecessary Using

--- MikouTools/Collections/Comparer/IReversibleComparer.cs
+++ MikouTools/Collections/Comparer/IReversibleComparer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MikouTools.Collections.Comparer
+﻿namespace MikouTools.Collections.Comparer
 {
     public interface IReversibleComparer<T> : IComparer<T>
     {
