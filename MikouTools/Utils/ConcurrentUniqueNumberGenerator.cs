161b8af94f6582ca0f1b445c2f3c04cfb22149e3
Mikou2761210 <mikou.2761210@gmail.com>
2025-03-20 23:18:50 +0900

    ConcurrentUniqueNumberGenerator Add

---
+++ MikouTools/Utils/ConcurrentUniqueNumberGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikouTools.Utils
+{
+    public class ConcurrentUniqueNumberGenerator : UniqueNumberGenerator
+    {
+        private readonly object _lock = new();
+
+        public ConcurrentUniqueNumberGenerator(IEnumerable<int>? initialUsedIDs = null) : base(null)
+        {
+            lock (_lock)
+                if (initialUsedIDs != null)
+                    foreach (int id in initialUsedIDs) base.TryClaimUniqueNumber(id);
+        }
+        public new int GenerateUniqueNumber()
+        {
+            lock(_lock) return base.GenerateUniqueNumber();
+        }
+
+        public new void ReleaseUniqueNumber(int id)
+        {
+            lock (_lock) base.ReleaseUniqueNumber(id);
+        }
+
+        public new bool TryClaimUniqueNumber(int id)
+        {
+            lock (_lock) return base.TryClaimUniqueNumber(id);
+        }
+    }
+}
