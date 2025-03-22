471848fd105dbf96457181bbeb0081d9ba475fc3
Mikou2761210 <mikou.2761210@gmail.com>
2025-03-22 12:36:31 +0900

    EntityRepository Improvement

--- MikouTools/Collections/Specialized/EntityTracking/EntityRepository.cs
+++ MikouTools/Collections/Specialized/EntityTracking/EntityRepository.cs
@@ -45,6 +45,8 @@ namespace MikouTools.Collections.Specialized.EntityTracking
 
         public virtual bool TryGet(int id, [MaybeNullWhen(false)] out T item) => _collections.TryGetValue(id, out item);
 
+        public virtual T? TryGet(int id) => TryGet(id, out T? item) ? item : item;
+
         public virtual bool TryAdd(T item) => _collections.TryAdd(item.ID, item);
 
         public virtual bool Remove(int id)
